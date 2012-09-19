fe34a8ac6b78d8852d48ec9c1d99fc655e013397
Taito Horiuchi <taito.horiuchi@gmail.com>
2012-09-20 00:44:06 +0300

    Release 0.5.1

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ long_description = (
 
 setup(
     name='collective.cart.core',
-    version='0.5',
+    version='0.5.1',
     description="Yet another cart for Plone.",
     long_description=long_description,
     # Get more strings from
