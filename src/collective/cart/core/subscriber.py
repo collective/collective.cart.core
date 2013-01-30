22883a7cbf284f98efbe5da3fd15469904d6dd94
Taito Horiuchi <taito.horiuchi@gmail.com>
2013-01-30 07:14:27 +0200

    Moved subscriber: return_stock_to_original to collective.cart.shopping.

--- src/collective/cart/core/subscriber.py
+++ src/collective/cart/core/subscriber.py
@@ -1,12 +1,7 @@
 from Acquisition import aq_inner
 from Acquisition import aq_parent
-from Products.CMFCore.interfaces import IActionSucceededEvent
 from Products.statusmessages.interfaces import IStatusMessage
-from collective.behavior.stock.interfaces import IStock
 from collective.cart.core import _
-from collective.cart.core.interfaces import ICart
-from collective.cart.core.interfaces import ICartAdapter
-from collective.cart.core.interfaces import ICartArticleAdapter
 from collective.cart.core.interfaces import ICartContainer
 from collective.cart.core.interfaces import IMakeShoppingSiteEvent
 from collective.cart.core.interfaces import IShoppingSite
@@ -36,14 +31,3 @@ def add_cart_container(event):
             context, 'collective.cart.core.CartContainer',
             id="cart-container", title="Cart Container", checkConstraints=False)
         modified(container)
-
-
-@grok.subscribe(ICart, IActionSucceededEvent)
-def return_stock_to_original(context, event):
-    if event.action == 'canceled':
-        for carticle in ICartAdapter(context).articles:
-            obj = carticle['obj']
-            article = ICartArticleAdapter(obj).orig_article
-            if article:
-                IStock(article).add_stock(obj.quantity)
-                modified(article)
