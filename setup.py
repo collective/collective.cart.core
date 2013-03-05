c8d87cefa057cdda1769ca122532682b756543d8
Taito Horiuchi <taito.horiuchi@gmail.com>
2013-03-05 18:45:54 +0200

    Release 0.5.3

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ long_description = (
 
 setup(
     name='collective.cart.core',
-    version='0.5.2.13',
+    version='0.5.3',
     description="Yet another cart for Plone.",
     long_description=long_description,
     classifiers=[
